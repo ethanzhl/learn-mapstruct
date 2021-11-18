66887deaa771e9f3a47e70711cd731902c5fb231
ethanzhl <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>
2021-11-18 15:02:02 +0800

    feat(*): init

--- src/main/java/com/ethan/learn/mapstruct/entity/dto/UserDto.java
+++ src/main/java/com/ethan/learn/mapstruct/entity/dto/UserDto.java
@@ -1,20 +1,48 @@
 package com.ethan.learn.mapstruct.entity.dto;
 
 
-import lombok.Builder;
-import lombok.Data;
-
 import java.time.LocalDateTime;
 
 /**
  * @author ethan
  * @date 2021/10/12 2:22 下午
  */
-@Data
-@Builder
+// @Data
 public class UserDto {
     private Long userId;
     private String userName;
     private String sex;
     private LocalDateTime createTime;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
 }
--- src/main/java/com/ethan/learn/mapstruct/entity/po/UserPo.java
+++ src/main/java/com/ethan/learn/mapstruct/entity/po/UserPo.java
@@ -1,20 +1,48 @@
 package com.ethan.learn.mapstruct.entity.po;
 
 
-import lombok.Builder;
-import lombok.Data;
-
 import java.time.LocalDateTime;
 
 /**
  * @author ethan
  * @date 2021/10/12 2:22 下午
  */
-@Data
-@Builder
+// @Data
 public class UserPo {
     private Long id;
     private String name;
     private String sex;
     private LocalDateTime createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
 }
